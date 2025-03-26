2b1bff33b3f4a8393b32dd9d7b43d1de378223e0
woo-dev-log <sswwhh.dev@gmail.com>
2025-03-26 17:15:57 +0900

    프리패칭, css module

--- onebite-next/section02/src/pages/_app.tsx
+++ onebite-next/section02/src/pages/_app.tsx
@@ -1,11 +1,33 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  const onclickButton = () => {
+    router.push("/test");
+  };
+
+  useEffect(() => {
+    router.prefetch("test");
+  }, []);
+
   return (
     <>
-      <header>글로벌 헤더</header>
+      <header>
+        <Link href={"/"}>index</Link>
+        &nbsp;
+        <Link href={"/search"} prefetch={false}>search</Link>
+        &nbsp;
+        <Link href={"/book/1"}>book/1</Link>
+        <div>
+          <button onClick={onclickButton}>/test 페이지로 이동</button>
+        </div>
+      </header>
       <Component {...pageProps} />
     </>
-  )
+  );
 }
