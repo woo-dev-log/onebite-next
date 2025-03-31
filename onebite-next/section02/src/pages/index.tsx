a7ce99266ce8cb1d7a25c4e2bd491c86553c9539
woo-dev-log <sswwhh.dev@gmail.com>
2025-03-31 12:02:29 +0900

    SSR 데이터페칭

--- onebite-next/section02/src/pages/index.tsx
+++ onebite-next/section02/src/pages/index.tsx
@@ -1,19 +1,39 @@
 import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.css";
-import { ReactNode } from "react";
-import books from '@/mock/books.json';
+import { ReactNode, useEffect } from "react";
 import BookItem from "@/components/book-item";
+import { InferGetServerSidePropsType } from "next";
+import fetchBooks from "@/lib/fetch-books";
+import fetchRandomBooks from "@/lib/fetch-random-books";
 
-export default function Home() {
+export const getServerSideProps = async () => {
+  const [allBooks, recoBooks] = await Promise.all([
+    fetchBooks(),
+    fetchRandomBooks()
+  ]);
+
+  return {
+    props: {
+      allBooks,
+      recoBooks
+    }
+  };
+}
+
+export default function Home({
+  allBooks,
+  recoBooks
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => <BookItem key={book.id} {...book} />)}
+        {recoBooks.map((book) => <BookItem key={book.id} {...book} />)}
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => <BookItem key={book.id} {...book} />)}
+        {allBooks.map((book) => <BookItem key={book.id} {...book} />)}
       </section>
     </div>
   );
