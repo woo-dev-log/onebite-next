344f7d821498b46f5f6d043b98d4454916199ec1
woo-dev-log <sswwhh.dev@gmail.com>
2025-04-04 17:56:57 +0900

    서버 컴포넌트

--- onebite-next/section03/src/app/(with-searchbar)/layout.tsx
+++ onebite-next/section03/src/app/(with-searchbar)/layout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import Searchbar from "./searchbar";
 
 export default function Layout({
   children
@@ -7,7 +8,7 @@ export default function Layout({
 }) {
   return (
     <>
-      <div>임시 서치바</div>
+      <Searchbar />
       {children}
     </>
   );
--- onebite-next/section03/src/app/(with-searchbar)/page.tsx
+++ onebite-next/section03/src/app/(with-searchbar)/page.tsx
@@ -1,7 +1,14 @@
+import ClientComponent from "./client-component";
 import styles from "./page.module.css";
+import ServerComponent from "./servaer-component";
 
 export default function Home() {
   return (
-    <div className={styles.page}>인덱스 페이지</div>
+    <div className={styles.page}>
+      인덱스 페이지
+      <ClientComponent>
+        <ServerComponent />
+      </ClientComponent>
+    </div>
   );
 }
