b318449092f62e4a24ceec5659662d22d6d9da17
woo-dev-log <sswwhh.dev@gmail.com>
2025-04-11 17:36:31 +0900

    데이터 페칭 코드 수정중

---
+++ onebite-next/section04/src/app/(with-searchbar)/page.tsx
@@ -0,0 +1,52 @@
+import BookItem from "@/components/book-item";
+import style from "./page.module.css";
+import { BookData } from "@/types";
+
+async function RecoBooks() {
+  const response = await fetch(`http://localhost:12345/book/random`);
+  if (!response.ok) {
+    return <div>오류가 발생했습니다 ...</div>
+  }
+
+  const recoBooks: BookData[] = await response.json();
+
+  return (
+    <>
+      {recoBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </>
+  );
+}
+
+async function AllBooks() {
+  const response = await fetch(`http://localhost:12345/book`);
+  if (!response.ok) {
+    return <div>오류가 발생했습니다 ...</div>
+  }
+
+  const allBooks: BookData[] = await response.json();
+
+  return (
+    <>
+      {allBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </>
+  );
+}
+
+export default async function Home() {
+  return (
+    <div className={style.container}>
+      <section>
+        <h3>지금 추천하는 도서</h3>
+        <RecoBooks />
+      </section>
+      <section>
+        <h3>등록된 모든 도서</h3>
+        <AllBooks />
+      </section>
+    </div>
+  );
+}
