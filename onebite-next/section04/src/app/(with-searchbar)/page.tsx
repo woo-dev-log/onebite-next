215b3c930de384b06e6fe525b9a3f7de95b67f29
woo-dev-log <sswwhh.dev@gmail.com>
2025-04-16 17:44:44 +0900

    데이터 캐싱 및 리퀘스트 메모이제이션

--- onebite-next/section04/src/app/(with-searchbar)/page.tsx
+++ onebite-next/section04/src/app/(with-searchbar)/page.tsx
@@ -3,7 +3,10 @@ import style from "./page.module.css";
 import { BookData } from "@/types";
 
 async function RecoBooks() {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`);
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
+    { next: { revalidate: 3 } }
+  );
   if (!response.ok) {
     return <div>오류가 발생했습니다 ...</div>
   }
@@ -20,7 +23,10 @@ async function RecoBooks() {
 }
 
 async function AllBooks() {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`);
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
+    { cache: "no-store" }
+  );
   if (!response.ok) {
     return <div>오류가 발생했습니다 ...</div>
   }
--- onebite-next/section04/src/app/layout.tsx
+++ onebite-next/section04/src/app/layout.tsx
@@ -1,6 +1,26 @@
 import "./globals.css";
 import Link from "next/link";
 import style from "./layout.module.css";
+import { BookData } from "@/types";
+
+async function Footer() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`
+  );
+
+  if (!response.ok) {
+    return <footer>제작 @woo</footer>;
+  }
+
+  const books: BookData[] = await response.json();
+  const bookCount = books.length;
+  return (
+    <footer>
+      <div>제작 @woo</div>
+      <div>현재 {bookCount}개의 도서가 등록되어 있습니다.</div>
+    </footer>
+  );
+}
 
 export default function RootLayout({
   children,
@@ -15,7 +35,7 @@ export default function RootLayout({
             <Link href={"/"}>📚 ONEBITE BOOKS</Link>
           </header>
           <main>{children}</main>
-          <footer>제작 @woo</footer>
+          <Footer />
         </div>
       </body>
     </html>
